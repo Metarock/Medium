5429ce7d538d82b17573d5f06c583d50101371cc
Metarock <esang037@gmail.com>
2022-01-23 14:52:14 +1300

    header start

---
+++ components/Header.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import React, { useState } from 'react';
+
+export const Header: React.FC = ({}) => {
+    return (
+        <header>
+            <div className='flex items-center space-x-5'>
+                <Link href='/'>
+                    <img
+                        className='w-44 object-contain cursor-pointer'
+                        src='https://miro.medium.com/max/8978/1*s986xIGqhfsN8U--09_AdA.png'
+                        alt=''
+                    />
+                </Link>
+                {/* tags */}
+                <div className='hidden md:inline-flex items-center space-x-5'>
+                    <h3>About</h3>
+                    <h3>Contact</h3>
+                    <h3 className='text-white bg-green-600 px-4 py-1 rounded-full'>
+                        Follow
+                    </h3>
+                </div>
+            </div>
+            <div></div>
+        </header>
+    );
+};
